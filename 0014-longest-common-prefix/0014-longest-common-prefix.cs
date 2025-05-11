591b5809e6b8aa24e5417a88fe853d03746818d4
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-05-12 02:48:57 +0300

    Time: 1 ms (89.27%), Space: 43.3 MB (53.07%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cs
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cs
@@ -1,18 +1,14 @@
 public class Solution {
     public string LongestCommonPrefix(string[] strs) {
-        
-        int n=strs.Length;
-        if(n<=1)return strs[0];
-        Array.Sort(strs);
-        int m=strs[0].Length;
+        int n=strs.Length,m=201;
         string res="";
-        //foreach(var s in strs)m=Math.Min(m,s.Length);
+        foreach(var s in strs)m=Math.Min(m,s.Length);
         for(int j=0;j<m;j++)
         {
             
             char num=strs[0][j];
-
-            for(int i=0;i<n;i++)
+            int i=0;
+            for(;i<n;i++)
             {
                 if(num!=strs[i][j])return res;
             }
