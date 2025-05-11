1e9a059a20454ce8def0c07b38e9edba20eebcc2
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-05-12 02:42:25 +0300

    Time: 5 ms (72.36%), Space: 43.9 MB (36.98%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -1,13 +1,17 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
         if(s.Length!=t.Length)return false;
-        char[] arr1=s.ToCharArray();
-        char[] arr2=t.ToCharArray();
-        Array.Sort(arr1);
-        Array.Sort(arr2);
-        string x=new string(arr1);
-        string y=new string(arr2);
-        if(String.Compare(x,y)==0)return true;
-        return false;
+        Dictionary<char,int> freq=new Dictionary<char,int>();
+        foreach(var i in s)
+        {
+            if(freq.ContainsKey(i))freq[i]++;
+            else freq[i]=1;
+        }
+        foreach(var i in t)
+        {
+            if(freq.ContainsKey(i)&&freq[i]>0)freq[i]--;
+            else return false;
+        }
+        return true;
     }
 }
