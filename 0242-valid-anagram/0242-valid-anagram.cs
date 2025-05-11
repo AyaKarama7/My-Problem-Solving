cdf4deaf44287457638c0d3968e35bba3f716f41
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-05-12 02:36:59 +0300

    Time: 32 ms (25.5%), Space: 44.7 MB (12.71%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        char[] arr1=s.ToCharArray();
+        char[] arr2=t.ToCharArray();
+        Array.Sort(arr1);
+        Array.Sort(arr2);
+        string x=new string(arr1);
+        string y=new string(arr2);
+        if(String.Compare(x,y)==0)return true;
+        return false;
+    }
+}
