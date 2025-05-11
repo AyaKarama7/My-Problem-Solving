fc31b54cac79597df53737250b9959fb427119a0
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-05-11 22:22:23 +0300

    Time: 11 ms (5.19%), Space: 62.2 MB (16.72%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int n=prices.Length;
+        int[] maxNext=new int[n];
+        maxNext[n-1]=prices[n-1];
+        int mx=0;
+        for(int i=n-2;i>=0;i--)
+        {
+            maxNext[i]=Math.Max(maxNext[i+1],prices[i]);
+            mx=Math.Max(mx,maxNext[i]-prices[i]);
+        }
+        return mx;
+    }
+}
