bc18194c89ed8759951c0511d6d482bc314d0c36
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-05-12 04:30:04 +0300

    Time: 4 ms (59.47%), Space: 49.8 MB (51.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.cs
+++ 0169-majority-element/0169-majority-element.cs
@@ -1,6 +1,18 @@
 public class Solution {
     public int MajorityElement(int[] nums) {
-        Array.Sort(nums);
-        return nums[nums.Length/2];
+        Dictionary<int,int>freq=new Dictionary<int,int>();
+        foreach(var i in nums)
+        {
+            if(freq.ContainsKey(i))
+            {
+                freq[i]++;
+            }
+            else freq[i]=1;
+        }
+        foreach(var i in freq)
+        {
+            if(i.Value>nums.Length/2)return i.Key;
+        }
+        return 0;
     }
 }
