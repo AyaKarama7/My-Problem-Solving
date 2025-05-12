25b825ab5e87c1a47aff15bfcea055b571060096
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-05-12 03:51:02 +0300

    Time: 1 ms (96.38%), Space: 58.6 MB (26.5%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cs
+++ 0283-move-zeroes/0283-move-zeroes.cs
@@ -1,10 +1,11 @@
 public class Solution {
     public void MoveZeroes(int[] nums) {
-        int pos=0;
-        for(int i=0;i<nums.Length;i++)
+        int pos=0,n=nums.Length;
+        if(n==1)n=-1;
+        for(int i=0;i<n;i++)
         {
             if(nums[i]!=0)nums[pos++]=nums[i];
         }
-        while(pos<nums.Length)nums[pos++]=0;
+        while(pos<n)nums[pos++]=0;
     }
 }
