842b6969d0ff158d0e7985dcb80f70dcf0f32dfc
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-05-12 04:20:19 +0300

    Time: 22 ms (14.98%), Space: 50.1 MB (12.5%) - LeetHub

--- 0169-majority-element/0169-majority-element.cs
+++ 0169-majority-element/0169-majority-element.cs
@@ -2,7 +2,7 @@ public class Solution {
     public int MajorityElement(int[] nums) {
         Array.Sort(nums);//2 3 3 
         int n=nums.Length;//3
-        for(int i=0;i<=n-n/2;i++)
+        for(int i=0;i<n-n/2;i++)
         {
             if(nums[i]==nums[i+n/2])return nums[i];
         }
