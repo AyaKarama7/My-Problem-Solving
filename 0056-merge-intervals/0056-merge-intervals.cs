8157257d89ad0b7dca79f71b4432e08239d7728a
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-07-28 02:14:33 +0300

    Time: 4 ms (99.01%), Space: 57 MB (31.42%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cs
+++ 0056-merge-intervals/0056-merge-intervals.cs
@@ -1,33 +1,25 @@
 public class Solution {
     public int[][] Merge(int[][] intervals) {
-        if (intervals == null || intervals.Length == 0)
-            return new int[0][];
         Array.Sort(intervals,(a, b) => a[0].CompareTo(b[0]));
         List<int[]>ans=new List<int[]>();
         int L=intervals[0][0];
         int R=intervals[0][1];
         for(int i=1;i<intervals.Length;i++)
         {
-            if(intervals[i][0]<=R)
+            int a=intervals[i][0];
+            int b=intervals[i][1];
+            if(a<=R)
             {
-                if(intervals[i][1]>R)R=intervals[i][1];
+                if(b>R)R=b;
             }
             else 
             {
                 ans.Add(new int[]{L,R});
-                L=intervals[i][0];
-                R=intervals[i][1];
+                L=a;
+                R=b;
             }
         }
         ans.Add(new int[]{L,R});
-        // int[][]res=new int[ans.Count][];
-        // int idx=0;
-        // foreach(var i in ans)
-        // {
-        //     res[idx]=new int[2];
-        //     res[idx][0]=i[0];
-        //     res[idx++][1]=i[1];
-        // }
         return ans.ToArray();
     }
 }
