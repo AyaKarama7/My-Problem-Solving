08811c1fc5fb4beb94071a2181a1750c437a284c
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-08-09 11:40:48 +0300

    Time: 10 ms (83.18%), Space: 44.5 MB (55.3%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int EvalRPN(string[] tokens) {
+        Stack<int> st=new Stack<int>();
+        foreach(var i in tokens)
+        {
+            int x;
+            if(int.TryParse(i,out x))
+            {
+                st.Push(x);
+            }
+            else if(st.Count>1)
+            {
+                int num1=st.Peek();
+                st.Pop();
+                int num2=st.Peek();
+                st.Pop();
+                if(i=="+")num2+=num1;
+                else if(i=="-")num2-=num1;
+                else if(i=="*")num2*=num1;
+                else num2/=num1;
+                st.Push(num2);
+            }
+        }
+        return st.Peek();
+    }
+}
