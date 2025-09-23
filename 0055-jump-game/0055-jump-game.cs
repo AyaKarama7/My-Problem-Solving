363f56b6195d61f1f565476c5079d3a1ff9db717
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-09-23 13:01:01 +0300

    Time: 2 ms (59.81%), Space: 61.6 MB (9.96%) - LeetHub

--- 0055-jump-game/0055-jump-game.cs
+++ 0055-jump-game/0055-jump-game.cs
@@ -1,7 +1,8 @@
 public class Solution {
     public bool CanJump(int[] nums) {
-        int mx=0;
-        for(int i=0;i<nums.Length;i++)
+        int mx=0,n=nums.Length;
+        if(n==1)return true;
+        for(int i=0;i<n;i++)
         {
             if(i>mx)return false;
             mx=Math.Max(mx,i+nums[i]);
