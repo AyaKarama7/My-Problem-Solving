b090b55e621d3848dc4404d924804f81462edec6
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-09-23 11:49:54 +0300

    Time: 2 ms (74.36%), Space: 39.5 MB (43.59%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cs
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cs
@@ -1,61 +1,39 @@
 public class Solution {
+    private List<int>SubString(string s)
+    {
+        int startIdx=0,dotIdx,num;
+        List<int>nums=new List<int>();
+        while((dotIdx=s.IndexOf('.',startIdx))!=-1)
+        {
+            num=int.Parse(s.Substring(startIdx,dotIdx-startIdx));
+            nums.Add(num);
+            startIdx=dotIdx+1;
+        }
+        num=int.Parse(s.Substring(startIdx));
+        nums.Add(num);
+        return nums;
+    }
     public int CompareVersion(string version1, string version2) {
-        List<double>nums1=new List<double>();
-        List<double>nums2=new List<double>();
-        double num=0,p=0;
-        foreach(var ch in version1)
-        {
-            if(ch=='.')
-            {
-                nums1.Add(num);
-                num=0;
-                p=0;
-            }
-            else
-            {
-                num=num*Math.Pow(10,p)+ch-'0';
-                if(num>0)p++;
-            }
-        }
-        nums1.Add(num);
-        num=0;p=0;
-        foreach(var ch in version2)
-        {
-            if(ch=='.')
-            {
-                nums2.Add(num);
-                num=0;
-                p=0;
-            }
-            else
-            {
-                num=num*Math.Pow(10,p)+ch-'0';
-                if(num>0)p++;
-            }
-        }
-        nums2.Add(num);
-        int n=Math.Min(nums1.Count,nums2.Count);
-        int i=0;
-        foreach(var ele in nums1)Console.WriteLine(ele);
-        foreach(var ele in nums2)Console.WriteLine(ele);
+        List<int>v1=SubString(version1);
 
-        while(i<n)
+        List<int>v2=SubString(version2);
+
+        int i=0,j=0;
+        while(i<v1.Count&&j<v2.Count)
         {
-            if(nums1[i]<nums2[i])return -1;
-            if(nums1[i]>nums2[i])return 1;
+            if(v1[i]<v2[i])return -1;
+            if(v1[i]>v2[i])return 1;
             i++;
+            j++;
         }
-        while(i<nums1.Count)
+        while(i<v1.Count)
         {
-            if(nums1[i]!=0)return 1;
-            i++;
+            if(v1[i++]>0)return 1;
         }
-        while(i<nums2.Count)
+        while(j<v2.Count)
         {
-            if(nums2[i]!=0)return -1;
-            i++;
+            if(v2[j++]>0)return -1;
         }
-        
         return 0;
     }
 }
