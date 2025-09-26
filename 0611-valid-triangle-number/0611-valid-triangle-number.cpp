8d2a001c452dd63f258db57712a3d2490a1d6309
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-09-26 14:24:25 +0300

    Time: 34 ms (45.52%), Space: 16.7 MB (42.88%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -4,13 +4,15 @@ public:
         int n=nums.size();
         int ans=0;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-2;i++)
         {
-            for(int j=i+1;j<n-1;j++)
+            int k=i+2;
+            for(int j=i+1;j<n-1&&nums[i]!=0;j++)
             {
                 int sum=nums[i]+nums[j];
-                int idx=lower_bound(nums.begin(), nums.end(), sum) - nums.begin();
-                ans+=(max(idx-(j+1),0));
+                while(k<n&&sum>nums[k])k++;
+                ans+=(k-j-1);
+                
             }
         }
         return ans;
