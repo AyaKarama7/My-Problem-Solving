59f55065145605a781661e95bf3370ef1897142a
AyaKarama7 <112559557+AyaKarama7@users.noreply.github.com>
2025-09-28 00:58:03 +0300

    Time: 2 ms (97.37%), Space: 42.4 MB (34.21%) - LeetHub

--- 0830-largest-triangle-area/0830-largest-triangle-area.cs
+++ 0830-largest-triangle-area/0830-largest-triangle-area.cs
@@ -1,18 +1,17 @@
 public class Solution {
     public double LargestTriangleArea(int[][] points) {
         int n=points.Length;
-        double Area=double.MinValue;
+        double Area=0;
         for(int i=0;i<n-2;i++)
         {
             for(int j=i+1;j<n-1;j++)
             {
                 for(int k=j+1;k<n;k++)
                 {
-                    double currentArea = 0.5 * Math.Abs(
-                        points[i][0] * (points[j][1] - points[k][1]) +
-                        points[j][0] * (points[k][1] - points[i][1]) +
-                        points[k][0] * (points[i][1] - points[j][1])
-                    );
+                    int x1 = points[i][0], y1 = points[i][1];
+                    int x2 = points[j][0], y2 = points[j][1];
+                    int x3 = points[k][0], y3 = points[k][1];
+                    double currentArea = 0.5 * Math.Abs(x1*(y2 - y3) + x2*(y3 - y1) + x3*(y1 - y2));
                     Area = Math.Max(Area, currentArea);
                 }
             }
